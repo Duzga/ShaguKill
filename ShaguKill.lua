ea7777383aa43e3c87ef293b61d72a4c75d3a641
Duzga <saybooboo@gmail.com>
2022-11-10 20:52:27 +0700

    Show message only after killing a mob
    Also code refactoring.

--- ShaguKill.lua
+++ ShaguKill.lua
@@ -1,34 +1,42 @@
-local ShaguKill = CreateFrame("Frame")
-ShaguKill:RegisterEvent("PLAYER_XP_UPDATE")
-ShaguKill:RegisterEvent("UNIT_PET_EXPERIENCE")
-ShaguKill:RegisterEvent("PLAYER_ENTERING_WORLD")
+local ShaguKill = {}
 
-ShaguKill:SetScript("OnEvent", function()
-  if event == "PLAYER_ENTERING_WORLD" then
-    ShaguKill.oldXP =  UnitXP("player")
+function ShaguKill:CHAT_MSG_COMBAT_HOSTILE_DEATH()
+  self.oldXP =  UnitXP("player")
+  self.slainTime = GetTime()
+end
 
-  elseif event == "PLAYER_XP_UPDATE" then
-    ShaguKill.curXP = UnitXP("player")
-    ShaguKill.maxXP = UnitXPMax("player")
-
-    if ShaguKill.oldXP ~= nil then
-      ShaguKill.difXP = ShaguKill.curXP - ShaguKill.oldXP
-      if ShaguKill.difXP > 0 then
-        ShaguKill.RemainingKills = ceil((ShaguKill.maxXP - ShaguKill.curXP)/ShaguKill.difXP)
-        UIErrorsFrame:AddMessage("next level: |cffffffaa~" .. ShaguKill.RemainingKills .. "|r kills.")
-      end   
+function ShaguKill:PLAYER_XP_UPDATE()
+  if not self.slainTime then return end
+  if GetTime() - self.slainTime < 1 then
+    local curXP = UnitXP("player")
+    local difXP = curXP - self.oldXP
+    if difXP > 0 then
+      local maxXP = UnitXPMax("player")
+      local remainingKills = ceil((maxXP - curXP)/difXP)
+      UIErrorsFrame:AddMessage("next level: |cffffffaa~" .. remainingKills .. "|r kills.")
     end
-    ShaguKill.oldXP = ShaguKill.curXP
-
-  elseif event == "UNIT_PET_EXPERIENCE" then
-    ShaguKill.petCurXP, ShaguKill.petMaxXP = GetPetExperience()
-    if ShaguKill.petOldXP ~= nil then
-      ShaguKill.petDifXP = ShaguKill.petCurXP - ShaguKill.petOldXP
-      if ShaguKill.petDifXP > 0 then
-        ShaguKill.petRemainingKills = ceil((ShaguKill.petMaxXP - ShaguKill.petCurXP)/ShaguKill.petDifXP)
-        UIErrorsFrame:AddMessage("pet: |cffffffaa~" .. ShaguKill.petRemainingKills .. "|r kills.")
-      end   
-    end
-    ShaguKill.petOldXP = ShaguKill.petCurXP
   end
-end)
+  self.slainTime = nil
+end
+
+function ShaguKill:UNIT_PET_EXPERIENCE()
+  local petCurXP, petMaxXP = GetPetExperience()
+  if self.petOldXP ~= nil then
+    local petDifXP = petCurXP - self.petOldXP
+    if petDifXP > 0 then
+      local petRemainingKills = ceil((petMaxXP - petCurXP)/petDifXP)
+      UIErrorsFrame:AddMessage("pet: |cffffffaa~" .. petRemainingKills .. "|r kills.")
+    end
+  end
+  self.petOldXP = petCurXP
+end
+
+local function handleEvents(eventHandler)
+  local EventManager = CreateFrame("Frame")
+  for event in pairs(eventHandler) do
+    EventManager:RegisterEvent(event)
+  end
+  EventManager:SetScript("OnEvent", function() eventHandler[event](eventHandler) end)
+end
+
+handleEvents(ShaguKill)
